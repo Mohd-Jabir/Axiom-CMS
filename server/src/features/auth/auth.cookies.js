79da6bdbe5f1c5c7cc562221bbbad1a0d10437e7
Mohd Jabir <157728756+Mohd-Jabir@users.noreply.github.com>
2026-08-24 11:51:01 +0530

    Update auth.cookies.js

--- server/src/features/auth/auth.cookies.js
+++ server/src/features/auth/auth.cookies.js
@@ -1,7 +1,7 @@
 export const refreshCookieOptions = {
   httpOnly: true,
-  secure: false,
-  sameSite: "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 30 * 24 * 60 * 60 * 1000,
   path: "/",
 };
